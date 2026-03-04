c785174b1c47ddeba098a4fce3ca4fac967fae10
JOEL-PC\joelg <joel.gibson20@gmail.com>
2026-03-04 15:20:12 +0000

    Create core-dispatchers module
    Adds custom dispatcher annotations for constructor injection in viewModels

---
+++ core-dispatchers/src/main/java/com/android/dispatchers/DispatcherQualifiers.kt
@@ -0,0 +1,15 @@
+package com.android.dispatchers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
