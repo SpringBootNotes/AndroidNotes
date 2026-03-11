ad853c79ebc76c4da0bb4383d0ca71852eada28c
JOEL-PC\joelg <joel.gibson20@gmail.com>
2026-03-11 14:05:02 +0000

    Add LoginState and LoginEvents

--- feature-login/src/main/java/com/android/login/view/LoginScreen.kt
+++ feature-login/src/main/java/com/android/login/view/LoginScreen.kt
@@ -4,9 +4,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.android.ui.R
 
 
 @Composable
@@ -16,11 +21,32 @@ public fun LoginScreenRoute() {
 
 @Composable
 private fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
-    Button(
-        onClick = { viewModel.login("test2@test.com", "Test123456!") },
-        modifier = Modifier.padding(20.dp)
-    ) {
-        Text("Login")
-    }
+    val state = viewModel.state.collectAsStateWithLifecycle()
 
+    LoginScreenContent(state = state.value)
+
+    LaunchedEffect(Unit) {
+        viewModel.events.collect { event ->
+            when (event) {
+                is LoginEvents.NavigateToNotes -> {
+                    // Navigate to notes screen
+                }
+
+                is LoginEvents.NavigateToSignUp -> {
+                    // Navigate to sign up screen
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun LoginScreenContent(state: LoginState) {
+
+}
+
+@Preview
+@Composable
+private fun LoginScreenContentPreview() {
+    LoginScreenContent(state = LoginState())
 }
--- feature-login/src/main/java/com/android/login/view/LoginViewModel.kt
+++ feature-login/src/main/java/com/android/login/view/LoginViewModel.kt
@@ -1,23 +1,50 @@
 package com.android.login.view
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.dispatchers.IoDispatcher
 import com.android.login.domain.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.flow.receiveAsFlow
 
 @HiltViewModel
 internal class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
 ): ViewModel() {
+    private val _state = MutableStateFlow(LoginState())
+    val state: StateFlow<LoginState> = _state.asStateFlow()
+
+    private val _events = Channel<LoginEvents>()
+    val events = _events.receiveAsFlow()
 
     internal fun login(email: String, password: String) = viewModelScope.launch(dispatcher) {
-       val result = loginUseCase(email, password)
-        Log.d("testing123", result.toString())
+
     }
+}
+
+internal data class LoginState(
+    val isLoading: Boolean = false,
+    val email: String? = null,
+    val password: String? = null,
+    val rememberMeIsChecked: Boolean = false,
+    val showInvalidCredentialsErrorDialog: Boolean = false,
+    val showNetworkErrorDialog: Boolean = false,
+    val showGenericErrorDialog: Boolean = false
+) {
+    private val emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
+    val emailIsValid: Boolean = email?.let { emailRegex.matches(email) } ?: false
+    val loginButtonEnabled: Boolean = !email.isNullOrBlank() && !password.isNullOrBlank() && emailIsValid
+}
+
+internal sealed class LoginEvents {
+    data object NavigateToNotes: LoginEvents()
+    data object NavigateToSignUp: LoginEvents()
 }
