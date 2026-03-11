9af2acda4d1f986aa431a8ed21a4d5b2fb455e8d
JOEL-PC\joelg <joel.gibson20@gmail.com>
2026-03-11 14:29:03 +0000

    Add LoginViewModel logic

--- feature-login/src/main/java/com/android/login/view/LoginViewModel.kt
+++ feature-login/src/main/java/com/android/login/view/LoginViewModel.kt
@@ -13,20 +13,82 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 internal class LoginViewModel @Inject constructor(
     private val loginUseCase: LoginUseCase,
     @IoDispatcher private val dispatcher: CoroutineDispatcher
-): ViewModel() {
+) : ViewModel() {
     private val _state = MutableStateFlow(LoginState())
     val state: StateFlow<LoginState> = _state.asStateFlow()
 
     private val _events = Channel<LoginEvents>()
     val events = _events.receiveAsFlow()
 
-    internal fun login(email: String, password: String) = viewModelScope.launch(dispatcher) {
+    internal fun onLoginButtonClicked(email: String, password: String) = viewModelScope.launch(dispatcher) {
+        _state.update { it.copy(isLoading = true) }
 
+        try {
+            val result = loginUseCase(email, password)
+
+            when (result) {
+                is LoginUseCase.LoginResult.Success -> _events.send(LoginEvents.NavigateToNotes)
+
+                is LoginUseCase.LoginResult.InvalidCredentialsError -> showInvalidCredentialsErrorDialog()
+
+                is LoginUseCase.LoginResult.NetworkError -> showNetworkErrorDialog()
+
+                is LoginUseCase.LoginResult.GenericError -> showGenericErrorDialog()
+            }
+        } finally {
+            _state.update { it.copy(isLoading = false) }
+        }
+    }
+
+    internal fun onEmailChanged(email: String) {
+        _state.update { it.copy(email = email) }
+    }
+
+    internal fun onPasswordChanged(password: String) {
+        _state.update { it.copy(password = password) }
+    }
+
+    internal fun onRememberMeClicked() {
+        _state.update { it.copy(rememberMeIsChecked = !it.rememberMeIsChecked) }
+    }
+
+    internal fun onSignUpClicked() {
+        viewModelScope.launch {
+            _events.send(LoginEvents.NavigateToSignUp)
+        }
+    }
+
+    private fun showInvalidCredentialsErrorDialog() {
+        _state.update { it.copy(showInvalidCredentialsErrorDialog = true) }
+    }
+
+    internal fun dismissInvalidCredentialsErrorDialog() {
+        _state.update { it.copy(showInvalidCredentialsErrorDialog = false) }
+    }
+
+    private fun showNetworkErrorDialog() {
+        _state.update { it.copy(showNetworkErrorDialog = true) }
+    }
+
+    internal fun dismissNetworkErrorDialog() {
+        _state.update { it.copy(showNetworkErrorDialog = false) }
+
+    }
+
+    private fun showGenericErrorDialog() {
+        _state.update { it.copy(showGenericErrorDialog = true) }
+    }
+
+    internal fun dismissGenericErrorDialog() {
+        _state.update {
+            it.copy(showGenericErrorDialog = false)
+        }
     }
 }
 
@@ -41,10 +103,11 @@ internal data class LoginState(
 ) {
     private val emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$".toRegex()
     val emailIsValid: Boolean = email?.let { emailRegex.matches(email) } ?: false
-    val loginButtonEnabled: Boolean = !email.isNullOrBlank() && !password.isNullOrBlank() && emailIsValid
+    val loginButtonEnabled: Boolean =
+        !email.isNullOrBlank() && !password.isNullOrBlank() && emailIsValid
 }
 
 internal sealed class LoginEvents {
-    data object NavigateToNotes: LoginEvents()
-    data object NavigateToSignUp: LoginEvents()
+    data object NavigateToNotes : LoginEvents()
+    data object NavigateToSignUp : LoginEvents()
 }
